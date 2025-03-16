b2cc66ee59d579fce174970155a797750d77bb24
iamalien123 <iamalien282@gmail.com>
2025-03-16 10:21:22 +0530

    Enable debug mode

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,13 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7d(c+!++0h-x)a6^1_m4y-i_xd%zd)knjdc)%(==9jt-jkbt$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = [
-    "myproject-e05p.onrender.com",
-    "127.0.0.1",
-    "localhost"
-]
 
 
 
