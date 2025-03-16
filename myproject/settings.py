b6b6ee6b420543e3705296aa3329aec00c1eebef
iamalien123 <iamalien282@gmail.com>
2025-03-16 10:23:10 +0530

    Fixed ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7d(c+!++0h-x)a6^1_m4y-i_xd%zd)knjdc)%(==9jt-jkbt$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ["*"]
-
-
+DEBUG = False
+ALLOWED_HOSTS = ["myproject-e05p.onrender.com"]
 
 
 
