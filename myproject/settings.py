95f18358e1c6ae21c427b4ca33ea1962f79d388e
iamalien123 <iamalien282@gmail.com>
2025-03-17 22:07:05 +0530

    LocalHost Added

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,8 +87,10 @@ DATABASES = {
 }
 
 dj_database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(dj_database_url)
+DATABASE_URL = os.environ.get('DATABASE_URL')  # Get database URL from environment
 
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 
 
 # Password validation
