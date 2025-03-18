9cf25b24a956107d6a8e54438415c0235a425d1b
iamalien123 <iamalien282@gmail.com>
2025-03-18 09:16:22 +0530

    Fixed database configuration issue with dj_database_url

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ["*"]
+
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 # Application definition
 
 INSTALLED_APPS = [
